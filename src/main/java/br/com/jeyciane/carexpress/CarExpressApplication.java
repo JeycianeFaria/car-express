a337a75c3b07a38f8f5cb85280f75ba52d69570d
Jeyciane Faria <jeycianedeveloper@gmail.com>
2023-01-25 14:55:59 -0300

    Primeiro commit

---
+++ src/main/java/br/com/jeyciane/carexpress/CarExpressApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeyciane.carexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jeyciane/carexpress/CarExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jeyciane.carexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
