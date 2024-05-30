69eecb87722df324a744c0dbae431c65c8dcc971
Jeyciane Faria <jeycianedeveloper@gmail.com>
2024-05-30 18:02:06 -0300

    :sparkles: feat: criar repository para solicitação de viagens

---
+++ src/main/java/br/com/jeyciane/carexpress/domain/TravelRequestRepository.java
@@ -0,0 +1,7 @@
+package br.com.jeyciane.carexpress.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TravelRequestRepository extends JpaRepository<TravelRequest, Long> {
+
+}
