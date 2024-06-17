adb47d94842fcdd988eb59afe2b66c8b9d4fd44b
Jeyciane Faria <jeycianedeveloper@gmail.com>
2024-06-17 20:59:48 -0300

    :sparkles: feat: adicionar metodo que lista as viagens proximas

--- src/main/java/br/com/jeyciane/carexpress/domain/TravelService.java
+++ src/main/java/br/com/jeyciane/carexpress/domain/TravelService.java
@@ -1,15 +1,23 @@
 package br.com.jeyciane.carexpress.domain;
 
+import br.com.jeyciane.carexpress.interfaces.outcoming.GMapsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class TravelService {
 
+    private static final int MAX_TRAVEL_TIME = 600;
+
     @Autowired
     TravelRequestRepository travelRequestRepository;
+    @Autowired
+    GMapsService gMapsService;
+
 
     public TravelRequest saveTravelRequest(TravelRequest travelRequest) {
 
@@ -17,6 +25,14 @@ public class TravelService {
         travelRequest.setCreationDate(new Date());
 
         return travelRequestRepository.save(travelRequest);
+    }
+
+    public List<TravelRequest> listNearbyTravelRequests(String currentAddress) {
+        List<TravelRequest> requests = travelRequestRepository.findByStatus(TravelRequestStatus.CREATED);
+
+        return requests.stream()
+                .filter(tr -> gMapsService.getDistanceBetweenAddresses(currentAddress, tr.getOrigin()) < MAX_TRAVEL_TIME)
+                .collect(Collectors.toList());
     }
 
 }
