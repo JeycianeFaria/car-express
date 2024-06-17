fbdd15e5674a40d71d5e7e397f3f0bc68b1c022d
Jeyciane Faria <jeycianedeveloper@gmail.com>
2024-06-17 20:58:15 -0300

    :sparkles: feat: adicionar metodo para buscar travelRequest pelo status

--- src/main/java/br/com/jeyciane/carexpress/domain/TravelRequestRepository.java
+++ src/main/java/br/com/jeyciane/carexpress/domain/TravelRequestRepository.java
@@ -2,6 +2,10 @@ package br.com.jeyciane.carexpress.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TravelRequestRepository extends JpaRepository<TravelRequest, Long> {
+
+    List<TravelRequest> findByStatus(TravelRequestStatus status);
 
 }
